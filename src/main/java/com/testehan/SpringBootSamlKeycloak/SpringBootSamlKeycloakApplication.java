13b43cb3315d2d6a944f5ae1b52d760f90572623
DanTestehan <tdan89@yahoo.com>
2023-08-22 20:19:19 +0100

    initial commit

---
+++ src/main/java/com/testehan/SpringBootSamlKeycloak/SpringBootSamlKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.testehan.SpringBootSamlKeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamlKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSamlKeycloakApplication.class, args);
+	}
+
+}
